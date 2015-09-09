c95ae3acf719350447488e90fee88c58336f7446
escribis <escribis0@gmail.com>
2015-09-09 22:26:56 +0200

    remove out of date call

--- 3.3/macros/__Sandbox.py
+++ 3.3/macros/__Sandbox.py
@@ -5,8 +5,7 @@ import os
 execfile(
     os.path.join(os.path.expanduser("~"), ".modelio", "pymodelioprofile",
                  "startup.py"))
-execfile(r'C:\DEV\PyModelio\demos\graphs_demos\graph.py')
-#PyModelioEnv.execute('sandbox.helloworld.macro_helloworld',[],DEBUG)
+PyModelioEnv.execute('sandbox.helloworld.macro_helloworld',[],DEBUG)
 #PyModelioEnv.execute('sandbox.textualtree_test.macro_treetester',["pymodelio.misc.textual_tree"],DEBUG)
 #PyModelioEnv.execute('sandbox.gui_test.macro_gui',[],DEBUG)
 #PyModelioEnv.execute('sandbox.visit_test.macro_visit',[],DEBUG)
